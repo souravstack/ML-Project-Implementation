a12d4e2e320d787c18f41db3a65421540fd7dc97
Sourav <souravahmed319@gmail.com>
2026-01-05 21:21:16 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sourav",
+    author_email="souravahmed319@gmail.com",
+    packages=find_packages()
+)
