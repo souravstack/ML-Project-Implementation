4612b7f4c9fd371dc4cc1570401b87719569d69f
Sourav <souravahmed319@gmail.com>
2026-01-12 17:21:56 +0600

    prediction and web app added

--- us_visa/entity/config_entity.py
+++ us_visa/entity/config_entity.py
@@ -70,3 +70,9 @@ class ModelPusherConfig:
     bucket_name: str = MODEL_BUCKET_NAME
     s3_model_key_path: str = MODEL_FILE_NAME
 
+
+
+@dataclass
+class USvisaPredictorConfig:
+    model_file_path: str = MODEL_FILE_NAME
+    model_bucket_name: str = MODEL_BUCKET_NAME
